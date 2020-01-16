c32149dede1e03e525796d13b92e8136acd21409
AnatolySP <anspesiv@yahoo.com>
2020-01-17 00:16:29 +0300

    auth started

--- hofvids/settings.py
+++ hofvids/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hofvids.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['halls/templates/halls',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hofvids/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+try:
+    from .localsettings import *
+except ImportError:
+    pass
