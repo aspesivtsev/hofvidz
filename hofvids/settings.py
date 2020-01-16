a4e0a730f940526d0762e64196f54f65f716b698
AnatolySP <anspesiv@yahoo.com>
2020-01-16 11:12:42 +0300

    minor dev

--- hofvids/settings.py
+++ hofvids/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'halls.apps.HallsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'halls',
+    #'halls',
 ]
 
 MIDDLEWARE = [
